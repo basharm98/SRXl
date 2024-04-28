3f9d46acabc615731c3470ab24c15e46fad7f16d
basharm98 <167342964+basharm98@users.noreply.github.com>
2024-04-28 23:17:22 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# SRXl
+# SRC KABER
+POWERFUL MUSIC BOT
+
+━━━━━━━━━━━━━━━━━━━━
+
+<h2 align="center">
+    ──「 SRC ✘ ᴍᴜsɪᴄ 」──
+</h2>
+
+<p align="center">
+  <img src="https://telegra.ph/file/d640c53f6910a6578fc49.png">
+</p>
+
+_**ᴀᴠᴀɪʟᴀʙʟᴇ ᴏɴ ᴛᴇʟᴇɢʀᴀᴍ ᴀs [ᴏʟɪᴠɪᴀ ✘ ᴍᴜsɪᴄ]([https://t.me/F_U_01]
---
+++ config.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/ca3507b28c6f7a98aa214.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/ca3507b28c6f7a98aa214.jpg")
+
+SESSION = getenv("SESSION", None)
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/F_U_01")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/F_U_01")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6776871146").split()))
+
+
+FAILED = "https://telegra.ph/file/ca3507b28c6f7a98aa214.jpg"
